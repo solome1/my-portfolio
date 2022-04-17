53fdcb8c7bb92f29885c53a80755e54764534110
solome <solomeyemane53@example.com>
2022-04-17 12:54:15 -0700

    modify my js and css

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-$(document).ready(function()
-{
-    $('menu').click(function()
-    {
-        $(this).toggleClass('fa-times');
-        $('header').toggleClass('toggle');
+$(document).ready(function () {
+    $("#menu").click(function () {
+        $(this).toggleClass("fa-times");
+        $("header").toggleClass("toggle");
     });
-    $(window).on('scroll load',function()
-    {
-        $('#menu').removeClass('fa-times');
-        $('header').removeClass('toggle')
+    $(window).on("scroll load", function() {
+         $("#menu").removeClass("fa-times");
+         $("header").removeClass("toggle");
 
-        if($(window).scrollTop() >   0)
-        {
-            $('.top').show();
-        }
+         if($(window).scrollTop() >   0)
+         {
+             $(".top").show();
+         }
         else
-        {
-            $('.top').hide();
-        }
-    });
-});    
+         {
+             $(".top").hide();
+         }
+     });
+ });    
